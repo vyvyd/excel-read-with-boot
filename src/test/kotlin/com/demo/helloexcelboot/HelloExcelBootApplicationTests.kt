41392f20dd01020af1810f214830b8a8a636d810
Vineeth Venudasan <vineethv@thoughtworks.com>
2023-10-24 07:51:33 +0200

    Initial commit

---
+++ src/test/kotlin/com/demo/helloexcelboot/HelloExcelBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.helloexcelboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloExcelBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
